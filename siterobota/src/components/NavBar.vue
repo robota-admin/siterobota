b4a761e692b9a248b25660250defc44daf08f032
EduSTC <edustclair@gmail.com>
2025-04-04 14:37:16 -0300

    projeto criado

---
+++ siterobota/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <div>
+            <a href=""><img src=""></a>
+            <h1>Robota</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+            </ul>
+        </nav>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
